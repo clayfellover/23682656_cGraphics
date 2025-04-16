1860c1d264144681d804476f1cd3bd99997fd12f
Clay Warden <claysonwarden@gmail.com>
2025-04-16 17:04:29 +0100

    cmake build
    compiled it all in cmake so i could actually write stuff

---
+++ build/CMakeFiles/launch-Computer_Graphics_Coursework-Release.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/clayw/Documents/GitHub/23682656_cGraphics/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\clayw\Documents\GitHub\23682656_cGraphics\build\Release\Computer_Graphics_Coursework.exe"  
+pause
